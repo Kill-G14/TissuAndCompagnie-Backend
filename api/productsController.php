3537cbc84039bb3d75b9c5950cd9eef917488b37
Killian <killian.godmont@gmail.com>
2025-05-27 19:19:58 +0200

    .

--- api/productsController.php
+++ api/productsController.php
@@ -7,19 +7,15 @@ require '../Src/Repositories/ProductsRepository.php';
 require '../Src/Services/productService.php';
 require '../Src/db.php';
 
-// Configuration DB
 $pdo = new PDO("mysql:host=localhost;dbname=tissuetcompagnie;charset=utf8", "root", "");
 
-// Récupération paramètres GET
 $type = $_GET['type'] ?? 'button';
 $page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
 $perPage = 28;
 
-// Appel des services
 $repo = new ProductsRepository($pdo);
 $service = new ProductsService($repo);
 $data = $service->getPaginatedProducts($type, $page, $perPage);
 
-// Réponse JSON
 header('Content-Type: application/json');
 echo json_encode($data);
