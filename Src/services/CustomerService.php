65f17b205157e89b9690c8802485e8241c309c46
Killian <killian.godmont@gmail.com>
2025-05-30 16:51:05 +0200

    customer

---
+++ Src/services/CustomerService.php
@@ -0,0 +1,19 @@
+<?
+
+namespace App\Services;
+
+use App\Repositories\CustomerRepository;
+
+class CustomerService {
+    private $repo;
+
+    public function __construct(CustomerRepository $repo) {
+        $this->repo = $repo;
+    }
+
+    public function getCustomerById($id) {
+        return $this->repo->getCustomerById($id);
+    }
+
+    
+}
